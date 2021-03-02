8027f86606e98bfe675b46df7d68d607d3454746
cwilliams366 <77814662+cwilliams366@users.noreply.github.com>
2021-03-02 02:14:55 -0500

    Update IntegerSetException.java

--- juniors-word-of-code/src/org/howard/edu/lsp/assignment4/implementation/IntegerSetException.java
+++ juniors-word-of-code/src/org/howard/edu/lsp/assignment4/implementation/IntegerSetException.java
@@ -5,10 +5,16 @@ package org.howard.edu.lsp.assignment4.implementation;
  */
 public class IntegerSetException extends Exception {
 
+	/**
+	 * Default constructor of the exception class
+	 */
 	public IntegerSetException(){
 		
 	}
-	
+	/** The constructor that takes in the specific message to be passed onto its parent class
+	 * 
+	 * @param message the error message to be shown when an exception is thrown
+	 */
 	public IntegerSetException(String message) {
 		super(message);
 	}
