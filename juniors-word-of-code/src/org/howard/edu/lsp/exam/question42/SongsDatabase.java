f20958c41e9652c28240f4b52ba3ecb08d7ad950
cwilliams366 <carleton.w.aarrow@gmail.com>
2021-04-21 13:01:21 -0400

    Question 42

---
+++ juniors-word-of-code/src/org/howard/edu/lsp/exam/question42/SongsDatabase.java
@@ -0,0 +1,44 @@
+package org.howard.edu.lsp.exam.question42;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class SongsDatabase {
+	private Map<String, Set<String>> map = 
+             new HashMap<String, Set<String>>();
+	Set <String> songs = new HashSet <String>();
+		/**
+		 *  Add a song title to a genre 
+		 */
+	public void addSong(String genre, String songTitle) {
+		Set <String> newSongList = new HashSet <String>();
+			if(map.containsKey(genre)) {
+				map.get(genre).add(songTitle);
+			} else {
+				newSongList.add(songTitle);
+				map.put(genre, newSongList);
+			}
+		}
+
+	/**
+	 *  Return the Set that contains all songs for a genre 
+	 */
+	public Set<String> getSongs(String genre) {
+		return map.get(genre);
+	}
+
+	/**
+	 *  Return genre, i.e., jazz, given a song title 
+	 */
+	public String getGenreOfSong(String songTitle) {
+		for(Entry<String, Set<String>> temp: map.entrySet()) {
+			if(temp.getValue().contains(songTitle)) {
+				return temp.getKey();
+			}
+		}
+		return null;
+	}
+}
---
+++ juniors-word-of-code/src/org/howard/edu/lsp/exam/question42/SongsDatabaseTest.java
@@ -0,0 +1,74 @@
+package org.howard.edu.lsp.exam.question42;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SongsDatabaseTest {
+	
+	SongsDatabase db;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		db = new SongsDatabase();
+	}
+
+	@Test
+	@DisplayName("Test for the addSong method")
+	void testAddSong() {
+		db.addSong("Rap","Savage");
+		db.addSong("Rap", "Gin and Juice");
+		db.addSong("Jazz", "Always There");
+	}
+	
+	@Test
+	@DisplayName("Test for the getSongs method")
+	void testGetSongs() {
+		//Set up test
+		testAddSong();
+		Set <String> temp = new HashSet<String>();
+		Set <String> rapSongs = new HashSet<String>();
+		//Test 1
+		temp.add("Savage");
+		temp.add("Gin and Juice");
+		rapSongs = db.getSongs("Rap");
+		assertEquals(temp,rapSongs);
+		//Test 2
+		temp.clear();
+		temp.add("Always There");
+		assertEquals(temp,db.getSongs("Jazz"));
+		//Test 3
+		db.addSong("Metal", "Dyer's Eve");
+		db.addSong("Metal", "Battery");
+		db.addSong("Metal", "Thunder Horse");
+		temp.clear();
+		temp.add("Dyer's Eve");
+		temp.add("Battery");
+		temp.add("Thunder Horse");
+		assertEquals(temp,db.getSongs("Metal"));
+	}
+	
+	@Test
+	@DisplayName("Test for the getGenreOfSong method")
+	void testGetGenreOfSong() {
+		//Set up
+	    testAddSong();
+		db.addSong("Metal", "Dyer's Eve");
+		db.addSong("Metal", "Battery");
+		db.addSong("Metal", "Thunder Horse");
+		//Test 1
+		assertEquals("Jazz",db.getGenreOfSong("Always There"));
+		//Test 2
+		assertEquals("Metal",db.getGenreOfSong("Dyer's Eve"));
+		//Test 3
+		assertEquals("Rap",db.getGenreOfSong("Gin and Juice"));
+	}
+	
+}
